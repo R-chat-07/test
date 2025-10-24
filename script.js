01459c10b85db3cc78e20be99878fcaeb4515c7d
R-chat-07 <abc.h2010.chat@gmail.com>
2025-10-24 21:04:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.querySelector("#button").addEventListener("click", function() {
-    const awnser = Math.random();
-    if(awnser < 0.5) {
+    if(Math.random < 0.5) {
         alert("ハズレ");
-        console.log("ハズレ");
     }
 })
