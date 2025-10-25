470d4f8dda36cf0bf5316cd79ae9b54b3754a87f
R-chat-07 <abc.h2010.chat@gmail.com>
2025-10-25 15:04:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello");
+function preview(obj, previewId) { 
+
+    let fileReader = new FileReader();
+    
+    fileReader.onload = (function () { 
+        document.getElementById(previewId).src = fileReader.result; 
+    }); 
+    
+    fileReader.readAsDataURL(obj.files[0]); 
+}
