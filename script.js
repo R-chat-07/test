0f998b8878ede4d15752ea5fefbe27df82a9ae97
R-chat-07 <abc.h2010.chat@gmail.com>
2025-10-25 19:00:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function preview(obj, previewId) {
     
     fileReader.readAsDataURL(obj.files[0]); 
 }
+
+function loadVideo(videoId) { 
+    const iframeHtml = ` 
+    <iframe 
+    width="100%" 
+    height="500" 
+    src="https://www.youtube.com/embed/${videoId}"
+    frameborder="0" 
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> 
+    </iframe>`; 
+    document.getElementById('videoContainer').innerHTML = iframeHtml; 
+}
+
+function loadVideo2(videoId) { 
+    const iframeHtml = ` 
+    <iframe 
+    width="100%" 
+    height="500" 
+    src="https://www.youtube.com/embed/${videoId}"
+    frameborder="0" 
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> 
+    </iframe>`; 
+    document.getElementById('videoContainer2').innerHTML = iframeHtml; 
+}
